1b299fd376bb9bfad1367e62abdd7002afe9de0f
Bleep0 <support@6b6t.org>
2021-03-03 09:29:40 -0800

    Fixed annotation

--- src/main/java/dev/bleepo/antibookban/events/AntiBookBan.java
+++ src/main/java/dev/bleepo/antibookban/events/AntiBookBan.java
@@ -1,11 +1,13 @@
 package dev.bleepo.antibookban.events;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEditBookEvent;
 
 import java.nio.charset.StandardCharsets;
 
 public class AntiBookBan implements Listener {
+    @EventHandler
     public void onBookEdit(PlayerEditBookEvent event) {
         for (String bookPage : event.getNewBookMeta().getPages()) {
             if (!StandardCharsets.ISO_8859_1.newEncoder().canEncode(bookPage)) {
